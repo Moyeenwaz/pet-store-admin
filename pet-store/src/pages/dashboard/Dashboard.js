ac7132dd6f473e87beee6651f23d2efea81cb1d3
Divine-oyegun <divineoyegun@gmail.com>
2022-08-06 00:47:24 +0100

    Dashboard

--- pet-store/src/pages/dashboard/Dashboard.js
+++ pet-store/src/pages/dashboard/Dashboard.js
@@ -7,7 +7,17 @@ export const Dashboard = () => {
     <div className="dashboard">
       <Sidebar />
       <div className="dashboard-wrapper">
-        <h1>This is the Dashboard Page</h1>
+        <h1>Dashboard</h1>
+        <button className="btnd total-pets">
+          <h3>Total Pets</h3>
+          <img src="images/pet paw.svg" alt="" />
+          150
+        </button>
+        <button className="btnd2 total-clients">
+          <h3>Total Clients</h3>
+          <img src="images/man face.svg" alt="" />
+          55
+        </button>
       </div>
     </div>
   );
